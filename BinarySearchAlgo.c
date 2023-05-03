10e820a6e1bb68fe947a185deff30a3ce0db061b
CGU <Mohameddeaf65@gmail.com>
2023-05-03 19:36:20 +0300

    Binary Search Algo

--- BinarySearchAlgo.c
+++ BinarySearchAlgo.c
@@ -1,48 +1,46 @@
-#include <stdio.h>
+int findKthElement(int arr1[], int m, int arr2[], int n, int k) {
+    if (m == 0) {
+        return arr2[k];
+    }
+    if (n == 0) {
+        return arr1[k];
+    }
 
-int kthElement(int arr1[], int arr2[], int m, int n, int k) {
-    int left1 = 0, right1 = m - 1, left2 = 0, right2 = n - 1;
+    if (k == 0) {
+        return arr1[0] < arr2[0] ? arr1[0] : arr2[0];
+    }
 
-    while (left1 <= right1 && left2 <= right2) {
-        int mid1 = left1 + (right1 - left1) / 2;
-        int mid2 = left2 + (right2 - left2) / 2;
-        int totalElements = mid1 + mid2 + 2;
+    int mid1 = (m - 1) / 2;
+    int mid2 = (n - 1) / 2;
 
-        if (totalElements <= k) {
-            if (arr1[mid1] < arr2[mid2]) {
-                k -= (mid1 - left1 + 1);
-                left1 = mid1 + 1;
-            }
-            else {
-                k -= (mid2 - left2 + 1);
-                left2 = mid2 + 1;
-            }
+    if ((mid1 + mid2) < k) {
+        if (arr1[mid1] > arr2[mid2]) {
+            return findKthElement(arr1, m, arr2 + mid2 + 1, n - mid2 - 1, k - (mid2 + 1));
         }
         else {
-            if (arr1[mid1] < arr2[mid2]) {
-                right2 = mid2 - 1;
-            }
-            else {
-                right1 = mid1 - 1;
-            }
+            return findKthElement(arr1 + mid1 + 1, m - mid1 - 1, arr2, n, k - (mid1 + 1));
         }
     }
-
-    if (left1 > right1) {
-        return arr2[left2 + k - 1];
-    }
     else {
-        return arr1[left1 + k - 1];
+        if (arr1[mid1] > arr2[mid2]) {
+            return findKthElement(arr1, mid1, arr2, n, k);
+        }
+        else {
+            return findKthElement(arr1, m, arr2, mid2, k);
+        }
     }
 }
 
 int main() {
-    int arr1[] = { 1, 3, 4, 5, 7, 9 };
-    int arr2[] = { 2, 4, 6, 8, 10 };
+    int arr1[] = { 1, 2, 3, 4, 5 };
+    int arr2[] = { 6, 7, 8, 9, 10 };
+    int k = 5;
     int m = sizeof(arr1) / sizeof(arr1[0]);
     int n = sizeof(arr2) / sizeof(arr2[0]);
-    int k = 5;
 
-    printf("kth element is: %d", kthElement(arr1, arr2, m, n, k));
+    int result = findKthElement(arr1, m, arr2, n, k - 1);
+
+    printf("The k'th element is %d\n", result);
+
     return 0;
 }
